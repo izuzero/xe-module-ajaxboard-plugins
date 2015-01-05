e01d9cbdb446b055bdfa058cb553f666229a55a5
Eunsoo Lee <izuzero@gmail.com>
2015-01-05 19:37:07 +0900

    #3 하위 모듈 호환성 유지

--- sketchbook5/js/plugin.js
+++ sketchbook5/js/plugin.js
@@ -66,99 +66,94 @@
 		else {
 			return;
 		}
-		var called = false;
+		var rc = global.reComment;
+		if (rc && $.isFunction(rc)) {
+			global.reComment = function (doc_srl, cmt_srl, edit_url) {
+				_recomment = true;
+				rc(doc_srl, cmt_srl, edit_url);
+			};
+		}
+		var dcma = global.doCallModuleAction;
+		if (dcma && $.isFunction(dcma)) {
+			var ccma = function (obj) {
+				if (obj.message !== "success") {
+					alert(obj.message);
+				}
+			};
+			global.doCallModuleAction = function (module, action, target_srl) {
+				var isAction = ["procDocumentVoteUp", "procDocumentVoteDown", "procDocumentDeclare", "procCommentVoteUp", "procCommentVoteDown", "procCommentDeclare"];
+				if ($.inArray(action, isAction) > -1) {
+					var params = {
+						target_srl: target_srl,
+						cur_mid: core.current_mid,
+						mid: core.current_mid
+					};
+					return exec_xml(module, action, params, ccma);
+				}
+				else if ($.isFunction(dcma)) {
+					return dcma(module, action, target_srl);
+				}
+			};
+		}
+		var cic = global.completeInsertComment;
+		if (cic && $.isFunction(cic)) {
+			global.completeInsertComment = function (obj) {
+				core.clearEditor();
+			};
+		}
+		$("#cmtPosition").on("click", ".fdb_nav [href]", function (e) {
+			var href = $(this).attr("href");
+			if (href.indexOf("#") > -1) {
+				href = href.substring(0, href.indexOf("#"));
+			}
+			var act = href.getQuery("act");
+			var comment_srl = href.getQuery("comment_srl");
+			if (comment_srl && act == "dispBoardDeleteComment") {
+				core.deleteComment(
+					comment_srl,
+					href,
+					function (response, status, xhr) {
+						alert(response.message);
+					},
+					function (comment_srl) {
+						dispCommentList();
+					}
+				);
+				return false;
+			}
+		})
+		.on("click", ".bd_pg [href], .cmt_pg [href]", function (e) {
+			var $this = $(this);
+			var href = $this.attr("href");
+			if (href.indexOf("#") > -1) {
+				href = href.substring(0, href.indexOf("#"));
+			}
+			var cpage = href.getQuery("cpage");
+			if (cpage) {
+				dispCommentListByCpage(cpage > 1 && $this.hasClass("direction") ? "" : cpage);
+				return false;
+			}
+		});
+		$("#re_cmt .close").on("click", function (e) {
+			_recomment = false;
+			$("#re_cmt").hide();
+			dispCommentList();
+		});
+		$("#re_cmt input[type='submit']").on("click", function (e) {
+			_recomment = false;
+			$("#re_cmt").hide();
+		});
+		$(".bd_lst_wrp").on("click", ".bd_pg [href]", function (e) {
+			var href = $(this).attr("href");
+			if (href.indexOf("#") > -1) {
+				href = href.substring(0, href.indexOf("#"));
+			}
+			var page = href.getQuery("page");
+			dispDocumentListByPage(page || 1);
+			return false;
+		});
 		core.insertTrigger("clearEditor", "after", function () {
 			$(".simple_wrt textarea[id*='editor_']").val("");
-		});
-		core.insertTrigger("events.connect", "after", function (type) {
-			if (called) return;
-			called = true;
-			var rc = global.reComment;
-			if (rc && $.isFunction(rc)) {
-				global.reComment = function (doc_srl, cmt_srl, edit_url) {
-					_recomment = true;
-					rc(doc_srl, cmt_srl, edit_url);
-				};
-			}
-			var dcma = global.doCallModuleAction;
-			if (dcma && $.isFunction(dcma)) {
-				var ccma = function (obj) {
-					if (obj.message !== "success") {
-						alert(obj.message);
-					}
-				};
-				global.doCallModuleAction = function (module, action, target_srl) {
-					var isAction = ["procDocumentVoteUp", "procDocumentVoteDown", "procDocumentDeclare", "procCommentVoteUp", "procCommentVoteDown", "procCommentDeclare"];
-					if ($.inArray(action, isAction) > -1) {
-						var params = {
-							target_srl: target_srl,
-							cur_mid: core.current_mid,
-							mid: core.current_mid
-						};
-						return exec_xml(module, action, params, ccma);
-					}
-					else if ($.isFunction(dcma)) {
-						return dcma(module, action, target_srl);
-					}
-				};
-			}
-			var cic = global.completeInsertComment;
-			if (cic && $.isFunction(cic)) {
-				global.completeInsertComment = function (obj) {
-					core.clearEditor();
-				};
-			}
-			$("#cmtPosition").on("click", ".fdb_nav [href]", function (e) {
-				var href = $(this).attr("href");
-				if (href.indexOf("#") > -1) {
-					href = href.substring(0, href.indexOf("#"));
-				}
-				var act = href.getQuery("act");
-				var comment_srl = href.getQuery("comment_srl");
-				if (comment_srl && act == "dispBoardDeleteComment") {
-					core.deleteComment(
-						comment_srl,
-						href,
-						function (response, status, xhr) {
-							alert(response.message);
-						},
-						function (comment_srl) {
-							dispCommentList();
-						}
-					);
-					return false;
-				}
-			})
-			.on("click", ".bd_pg [href], .cmt_pg [href]", function (e) {
-				var $this = $(this);
-				var href = $this.attr("href");
-				if (href.indexOf("#") > -1) {
-					href = href.substring(0, href.indexOf("#"));
-				}
-				var cpage = href.getQuery("cpage");
-				if (cpage) {
-					dispCommentListByCpage(cpage > 1 && $this.hasClass("direction") ? "" : cpage);
-					return false;
-				}
-			});
-			$("#re_cmt .close").on("click", function (e) {
-				_recomment = false;
-				$("#re_cmt").hide();
-				dispCommentList();
-			});
-			$("#re_cmt input[type='submit']").on("click", function (e) {
-				_recomment = false;
-				$("#re_cmt").hide();
-			});
-			$(".bd_lst_wrp").on("click", ".bd_pg [href]", function (e) {
-				var href = $(this).attr("href");
-				if (href.indexOf("#") > -1) {
-					href = href.substring(0, href.indexOf("#"));
-				}
-				var page = href.getQuery("page");
-				dispDocumentListByPage(page || 1);
-				return false;
-			});
 		});
 		core.insertTrigger("events.insertDocument", "before", function (obj) {
 			dispDocumentList();
